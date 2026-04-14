1e8b93eb39d2bdb9bd9e81e56dd0bdf1dba8fec0
Ryan Ramdehol <ravinashwork@gmail.com>
2026-04-14 15:39:08 -0400

    calarie calculation api working add sheety for google sheets record adder

--- main.py
+++ main.py
@@ -7,16 +7,11 @@ import httpx
 # import os
 # variable = os.getenv("<ENV VARIABLE>")
 # load_dotenv()
-# --------.env.json file access for JSON structure--------#
-.env.json file
-import json
-with open('.env.json') as f:
-    config = json.load(f)
 # ---------------------------- CONSTANTS ------------------------------- #
-CREDENTIALS = config.get("credentials")
-APP_ID = CREDENTIALS.get("x-app-id")
-API_KEY = CREDENTIALS.get("x-app-key")
-APP_URL = CREDENTIALS.get("url")
+
+
+
+
 # ---------------------------- GLOBAL VARIABLES ------------------------------- #
 
 # ---------------------------- FUNCTIONS ------------------------------- #
@@ -44,6 +39,40 @@ async def delete_data(client, url, params, payload, headers):
 # MAIN Function
 async def main():
     async with httpx.AsyncClient() as client:
-        pass
+        # Calorie API
+        calorie_url = config["request_type"]["post_request"]["add_exercise"]["url"]
+        calorie_params = config["request_type"]["post_request"]["add_exercise"]["params"]
+        calorie_payload = config["request_type"]["post_request"]["add_exercise"]["payload"]
+        calorie_headers = config["request_type"]["post_request"]["add_exercise"]["headers"]
+        calorie_calculator_response = await post_data(client, calorie_url, calorie_params, calorie_payload, calorie_headers)
+        exercise = calorie_calculator_response["exercises"][0]["name"]
+        duration = calorie_calculator_response["exercises"][0]["duration_min"]
+        calories = calorie_calculator_response["exercises"][0]["nf_calories"]
+        # Sheety API
+
+    return calorie_calculator_response
 # ---------------------------- UI SETUP ------------------------------- #
-# run_program = asyncio.run(main())
+user_input_query = input("What exercise did you do today? ")
+# user_input_weight = int(input("What is your weight? "))
+# user_input_height = int(input("What is your height? "))
+# user_input_age = int(input("What is your age? "))
+# user_input_gender = input("What is your gender? ")
+
+# --------100days.env.json file access for JSON structure--------#
+# 100days.env.json file
+import json
+with open('100days.env.json', "r") as f:
+    config = json.load(f)
+config["request_type"]["post_request"]["add_exercise"]["payload"]["query"] = user_input_query
+# config["request_type"]["post_request"]["add_exercise"]["payload"]["weight_kg"] = user_input_weight
+# config["request_type"]["post_request"]["add_exercise"]["payload"]["height_cm"] = user_input_height
+# config["request_type"]["post_request"]["add_exercise"]["payload"]["age"] = user_input_age
+# config["request_type"]["post_request"]["add_exercise"]["payload"]["gender"] = user_input_gender
+
+with open('100days.env.json', 'w') as f:
+    json.dump(config, f, indent=4)
+# print(config)
+
+asyncio.run(main())
+
+
